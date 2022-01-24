e6ea0155e0a50ea3ab4edc6c7ba6a841f84066b3
Markus Noack <mknoack@gmx.de>
2022-01-24 17:56:42 +0100

    implemented scanMods further but doesnt quite work because of IModFile type

--- src/main/java/com/chaos02/structuredmodloader/StructuredModLoader.java
+++ src/main/java/com/chaos02/structuredmodloader/StructuredModLoader.java
@@ -1,32 +1,27 @@
 package com.chaos02.structuredmodloader;
 
 import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.Reader;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Consumer;
+import java.util.function.Predicate;
 import java.util.stream.Stream;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-//read toml file yourself lazy bitch
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.stream.JsonReader;
-import com.google.gson.stream.JsonToken;
-import com.google.gson.stream.MalformedJsonException;
+import com.electronwill.nightconfig.core.ConfigSpec;
+import com.electronwill.nightconfig.core.ConfigSpec.CorrectionListener;
+import com.electronwill.nightconfig.core.file.FileConfig;
 
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 //import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.loading.FMLPaths;
 import net.minecraftforge.forgespi.locating.IModFile;
 import net.minecraftforge.forgespi.locating.IModLocator;
 
@@ -36,12 +31,16 @@ public class StructuredModLoader implements IModLocator {
 	private static final Logger LOGGER = LogManager.getLogger();
 	
 	// Mod paths
-	private List<Path> mods;
-	final static File CONFIGFILE = new File("config/StructuredModLoader.json");
+	final static File CONFIGFILE = new File(FMLPaths.CONFIGDIR + "StructuredModLoader.toml");
 	final static List<String> DEFAULTDIRS = Arrays.asList("ignore", "unstable", "disable", "");
 	final static int DEFAULTDEPTH = 3;
-	final static Gson GSON = new GsonBuilder().setPrettyPrinting().create();
-	SMLConfig configRW = null;
+	
+	private static final Predicate<Object> KeywordValidator = s -> s instanceof String;
+	private List<Path> mods;
+	private List<String> ignoreWords = null;
+	private int depth = 0;
+	private FileConfig config = FileConfig.of(CONFIGFILE);
+	ConfigSpec smlSpec = new ConfigSpec();
 	
 	public StructuredModLoader() {
 		mods = new ArrayList<>();
@@ -53,110 +52,76 @@ public class StructuredModLoader implements IModLocator {
 		
 	}
 	
+	public class KeywordValidator {
+		public boolean test(String toValidate) {
+			return true;
+			
+		}
+	}
+	
 	public void configProvider() {
-		LOGGER.info("StructuredModLoader installed!");
 		LOGGER.info("Loading Config now!");
 		
-		// ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON,
-		// SMLConfig.SML_SPEC, "StructuredModLoader.toml");
+		smlSpec.defineInRange("recurseDepth", 3, 0, 5);
+		smlSpec.defineList("ignoreKeywords", DEFAULTDIRS, KeywordValidator);
 		
-		/*
-			test if config exist && valid
-				
-			else
-				writeConfig();
-		 
-		 */
+		config.load();
+		if (!smlSpec.isCorrect(config)) {
+			CorrectionListener corrector = (action, path, incorrectValue, correctedValue) -> {
+				String pathString = String.join(",", path);
+				LOGGER.error("Config file is incorrect, correcting!");
+				LOGGER.error("Corrected {}: was {}, is now {}", pathString, incorrectValue, correctedValue);
+				config.save();
+			};
+			int corrections = smlSpec.correct(config, corrector);
+			LOGGER.info("Corrected {} errors.", corrections);
+		}
 		
-		/*
-		String[] ignorePath = new String[SMLConfig.ignoreDir.get().size()];
-		for (int i = 0; i < SMLConfig.ignoreDir.get().size(); i++)
-			ignorePath[i] = SMLConfig.ignoreDir.get().get(i);
-		LOGGER.info("Ignoring >" + String.join(",", ignorePath) + "< keyworsds	"); */
+		depth = config.get("recurseDepth");
+		ignoreWords = config.get("ignoredKeyWords");
+		
+		ignoreWords.removeAll(Arrays.asList("", null)); // removes empty fields
+		
+		LOGGER.info("Ignoring >" + String.join(",", ignoreWords) + "< keyworsds	");
 		LOGGER.info("Loading subfolders that are NOT in config!");
-		LOGGER.info("Successfully recursively loaded {}", String.join(", ", mods.toArray().toString()));
 		
 	}
 	
-	public SMLConfig readConfig() {
-		SMLConfig configRead = null;
-		try (Reader configReader = new FileReader(CONFIGFILE)) {
-			configRead = GSON.fromJson(configReader, SMLConfig.class);
-		} catch (IOException e) {
-			LOGGER.error("FAILED TO READ CONFIG; RETURNING NULL");
-			e.printStackTrace();
-		}
-		return configRead;
-	}
-	
-	public void writeConfig() {
-		writeConfig(SMLConfig.class);
-	}
-	
-	public void writeConfig(Class ConfigClass) { // fertig
-		@SuppressWarnings("deprecation")
-		Object obj;
-		try {
-			obj = ConfigClass.newInstance();
-		} catch (InstantiationException | IllegalAccessException e) {
-			LOGGER.info("Assuming default config");
-			obj = SMLConfig.class;
-		}
+	private void recurseJarLoader(File dir, int depth) {
 		
-		LOGGER.info("Writing CONFIGFILE to {}", CONFIGFILE.toString());
-		configRW = createConfObj();
-		try (FileWriter configWriter = new FileWriter(CONFIGFILE)) {
-			GSON.toJson(configRW, configWriter);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	private static SMLConfig createConfObj() { // fertig
-		SMLConfig configWrite = new SMLConfig();
-		configWrite.setDirs(DEFAULTDIRS);
-		configWrite.setDepth(DEFAULTDEPTH);
-		return configWrite;
-	}
-	
-	// uses
-	
-	private static boolean isJsonValid(final JsonReader jsonReader) throws IOException {
-		try {
-			JsonToken token;
-			loop: while ((token = jsonReader.peek()) != JsonToken.END_DOCUMENT && token != null) {
-				switch (token) {
-					case BEGIN_ARRAY:
-						jsonReader.beginArray();
-						break;
-					case END_ARRAY:
-						jsonReader.endArray();
-						break;
-					case BEGIN_OBJECT:
-						jsonReader.beginObject();
-						break;
-					case END_OBJECT:
-						jsonReader.endObject();
-						break;
-					case NAME:
-						jsonReader.nextName();
-						break;
-					case STRING:
-					case NUMBER:
-					case BOOLEAN:
-					case NULL:
-						jsonReader.skipValue();
-						break;
-					case END_DOCUMENT:
-						break loop;
-					default:
-						throw new AssertionError(token);
+		if (dir != FMLPaths.MODSDIR.get().toFile()) {
+			LOGGER.info("Getting Files in {}", dir.toString());
+			File[] subFiles = dir.listFiles(File::isFile);
+			for (int i2 = 0; i2 < subFiles.length; i2++) {
+				if (subFiles[i2].toString().toLowerCase().endsWith(".jar")) {
+					mods.add(subFiles[i2].toPath());
+				} else {
+					LOGGER.info("Skipped {} because of file extension", subFiles[i2].toString());
 				}
 			}
-			return true;
-		} catch (final MalformedJsonException ignored) {
-			return false;
+			LOGGER.info("Finished loading {}", dir.toString());
+		} else {
+			LOGGER.info("Started at {}", dir.toString());
 		}
+		
+		File[] subDirs = dir.listFiles(File::isDirectory);
+		if (subDirs.length > 0) {
+			if (depth >= 0) {
+				for (int j = 0; j < subDirs.length; j++) {
+					if (!ignoreWords.stream().anyMatch(subDirs[j].toString()::contains)) {
+						LOGGER.info("Searching for mods in >" + subDirs[j] + "<");
+						recurseJarLoader(subDirs[j], depth - 1);
+					} else {
+						LOGGER.info("Skipping \"{}\" because of config", subDirs[j]);
+					}
+				}
+			} else {
+				LOGGER.error("Skipping subdirectories of \"{}\". Please rearrange or configure higher depth!", dir);
+			}
+		} else {
+			LOGGER.debug("\"{}\" contains no sub directories.", dir);
+		}
+		
 	}
 	
 	// Implements
@@ -168,8 +133,11 @@ public class StructuredModLoader implements IModLocator {
 	
 	@Override
 	public List<IModFile> scanMods() {
-		LOGGER.debug("SML.scanMods()");
+		LOGGER.info("Structured Mod Loader installed!");
 		configProvider();
+		recurseJarLoader(new File(FMLPaths.MODSDIR.toString()), depth);
+		
+		LOGGER.info("Successfully recursively loaded\n{}", String.join(",\n", mods.toArray().toString()));
 		return new ArrayList<IModFile>();
 	}
 	
@@ -181,19 +149,19 @@ public class StructuredModLoader implements IModLocator {
 	@Override
 	public void scanFile(IModFile modFile, Consumer<Path> pathConsumer) {
 		// TODO Auto-generated method stub
-		LOGGER.debug("SML.scanFile()");
+		LOGGER.debug("SML.scanFile({}, {})", modFile, pathConsumer);
 	}
 	
 	@Override
 	public void initArguments(Map<String, ?> arguments) {
 		// TODO Auto-generated method stub
-		LOGGER.debug("SML.initArguments() {}", arguments);
+		LOGGER.debug("SML.initArguments({})", arguments);
 	}
 	
 	@Override
 	public boolean isValid(IModFile modFile) {
 		// TODO Auto-generated method stub
-		LOGGER.debug("SML.isValid()");
+		LOGGER.debug("SML.isValid({})", modFile);
 		return true;
 	}
 	
