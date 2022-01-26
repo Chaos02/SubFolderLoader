f9b56f0735ec8185b4085a8fbe6e6a727df73162
Markus Noack <mknoack@gmx.de>
2022-01-26 11:03:31 +0100

    Added Features: Load MC version dir only and subdirs; added class for display in Minecraft

---
+++ src/main/java/com/chaos02/structuredmodloader/SMLForgeMod.java
@@ -0,0 +1,39 @@
+package com.chaos02.structuredmodloader;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fml.loading.LogMarkers;
+
+@Mod("structuredmodloader")
+public class SMLForgeMod {
+	private static final Logger LOGGER = LogManager.getLogger();
+	
+	public SMLForgeMod() {
+		/* This doesnt work because of java.lang.NoClassDefFoundError: net/minecraftforge/fml/javafmlmod/FMLJavaModLoadingContext */
+		// Register the setup method for modloading
+		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+		// Register the enqueueIMC method for modloading
+		// FMLJavaModLoadingContext.get().getModEventBus().addListener(this::enqueueIMC);
+		// Register the processIMC method for modloading
+		// FMLJavaModLoadingContext.get().getModEventBus().addListener(this::processIMC);
+		
+		// Register ourselves for server and other game events we are interested
+		// in
+		MinecraftForge.EVENT_BUS.register(this);
+	}
+	
+	@SubscribeEvent
+	public void setup(final FMLCommonSetupEvent event) {
+		// some preinit code
+		LOGGER.info(LogMarkers.LOADING, "Hello world from [SML]!!!");
+		LOGGER.info(LogMarkers.SPLASH,
+				"A HUGE THANK YOU! to stiebi99#2124 again because literally without him this mod would never exist!");
+	}
+	
+}
